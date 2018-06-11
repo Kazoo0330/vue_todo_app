0de37eae954a8a342f31c919f69683d0fad68780
kazoo <kazuki_saito@diveintocode.jp>
2018-06-11 10:11:06 +0900

    API準備 Materializeの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'tasks/index'
+  end
+
+  namespace :api do
+    get 'tasks/create'
+  end
+
+  namespace :api do
+    get 'tasks/update'
+  end
 
   root to: 'home#index'
-
+  
+  namespace :api, format: 'json' do
+    resources :tasks, only: %i[index create update]
+  end
 end
