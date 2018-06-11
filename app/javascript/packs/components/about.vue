3f9de440e070526cf71305e2edcb94f4f47b9204
kazoo <kazuki_saito@diveintocode.jp>
2018-06-11 12:44:34 +0900

    Vue-Router実装完了

---
+++ app/javascript/packs/components/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>This is a sample todo application for using Vue.js with Rails</p>
+    <p>Sample code is <a href="https://github.com/naoki85/todo_app_with_vue_and_rails" target="_blank">here...!</a></p>
+  </div>
+</template>
